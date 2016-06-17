c33d63c6ca95e0f97b133ac605d8b63be4f39843
rem{e}koh <git@remekoh.me>
2016-06-17 15:40:29 -0500

    added contact list function

--- Spec.Sheet.md
+++ Spec.Sheet.md
@@ -6,7 +6,7 @@
 //Phone number
 //Address - Street, City, State and Zip Code
 //Add and Remove info (Extra)
-//Show contacts list below input  - names only
+//Show contacts list below input  - names only - DONE!
 //Can't add new contact without name entry - DONE!
 //Display names when clicked in contacts list
-//submitting form creates a new object
+//submitting form creates a new object - DONE!
--- js/contacts.js
+++ js/contacts.js
@@ -1,56 +1,71 @@
 'use strict';
 $(document).ready(function() {
-//Empty contacts array
-var contacts = [];
+    //Empty contacts array
+    var contacts = [];
 
-//Initial Object
-var emptyContact = {
-    first: "",
-    last: "",
-    phone: "",
-    street: "",
-    city: "",
-    state: "",
-    zip: ""
-}
-
-//FUNCTIONS
-//Adding a contact
-// var addContact = function() { ... }
-function addContact() {
-    console.log("hi")
-    var newContact = Object.create(emptyContact);
-    newContact.first = $("#first-name").val();
-    newContact.last = $("#last-name").val();
-    newContact.phone = $("#phone-number").val();
-    newContact.street = $("#street").val();
-    newContact.city = $("#city").val();
-    newContact.state = $("#state").val();
-    newContact.zip = $("#zipcode").val();
-    contacts.push(newContact);
-    console.log(newContact);
-    return newContact;
-}
-//On Click function populates the empty contacts and adds to contact array
-//Also empty contact list UL and append updated Contacts Array
-
-
-// always use this when you are listening to a form.
-$('form').submit(function(event) {
-    event.preventDefault();
-    $('.contact-list').empty();
-    addContact();
-    var i;
-    for (i = 0; i < contacts.length; i++) {
-        $('.contact-list').append("<li>" + contacts[i].first + " " + contacts[i].last + "</li>");
+    //Initial Object
+    var emptyContact = {
+        first: "",
+        last: "",
+        phone: "",
+        street: "",
+        city: "",
+        state: "",
+        zip: ""
     }
-    console.log(contacts)
-    // add a-link for selecting contacts
-})
-// var newContact = this.emptyContact.forEach(function (element) {
 
-// }
+    //FUNCTIONS
+    //Adding a contact
+    // var addContact = function() { ... }
+    function addContact() {
+        console.log("hi")
+        var newContact = Object.create(emptyContact);
+        newContact.first = $("#first-name").val();
+        newContact.last = $("#last-name").val();
+        newContact.phone = $("#phone-number").val();
+        newContact.street = $("#street").val();
+        newContact.city = $("#city").val();
+        newContact.state = $("#state").val();
+        newContact.zip = $("#zipcode").val();
+        contacts.push(newContact);
+        console.log(newContact);
+        return newContact;
+    }
+    //On Click function populates the empty contacts and adds to contact array
+    //Also empty contact list UL and append updated Contacts Array
+    // var conTACT = new.Contact.first + new.Contact.first;
+
+    // always use this when you are listening to a form.
+    $('form').submit(function(event) {
+        event.preventDefault();
+        $('.contact-list').empty();
+        addContact();
+        var i;
+        for (i = 0; i < contacts.length; i++) {
+            $('.contact-list').append("<li><a href='#display-area' class='contact-display' onClick='contactDisplay();' id='" + i + "'>" + contacts[i].first + " " + contacts[i].last + "</a></li>");
+        }
+        console.log('id')
+        console.log(contacts)
+        // add a-link for selecting contacts
+    })
+    // var newContact = this.emptyContact.forEach(function (element) {
+
+    // }
 });
 
 //Click function to select contacts from contact list and display in the contact info session
+ function contactDisplay() {
+    console.log(
+        var userAnswer = $('.answer-choices').find('input[name=choices]:checked;').val()
+    // $('.newContact.first').html('First Name:' + this.first);
+}
+
+
+
+
+
+
+
+
+
 //Removes from array and reprints ul
