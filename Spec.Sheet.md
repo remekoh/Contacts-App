70e938d24684422d77c95e4828c06ed97ec956ae
remekoh <code@remekoh.me>
2017-11-16 13:56:44 -0600

    updated dependencies and readme

--- Spec.Sheet.md
+++ Spec.Sheet.md
@@ -5,8 +5,9 @@
 //First and last name
 //Phone number
 //Address - Street, City, State and Zip Code
-//Add and Remove info (Extra)
 //Show contacts list below input  - names only - DONE!
 //Can't add new contact without name entry - DONE!
 //Display names when clicked in contacts list
 //submitting form creates a new object - DONE!
+
+//Add and Remove info (Extra)
--- js/contacts.js
+++ js/contacts.js
@@ -1,57 +1,57 @@
 'use strict';
 $(document).ready(function() {
-    //Empty contacts array
-    var contacts = [];
+	//Empty contacts array
+	var contacts = [];
 
-    //Initial Object
-    var emptyContact = {
-        first: 'Brad',
-        last: 'Pitt',
-        phone: '3105550000',
-        street: '123 Main Street',
-        city: 'Beverly Hills',
-        state: 'CA',
-        zip: '90210'
-    }
+	//Initial Object
+	var emptyContact = {
+		first: 'Brad',
+		last: 'Pitt',
+		phone: '3105550000',
+		street: '123 Main Street',
+		city: 'Beverly Hills',
+		state: 'CA',
+		zip: '90210'
+	}
 
-    //FUNCTIONS
-    //Adding a contact
-    function addContact() {
-        var newContact = Object.create(emptyContact);
-        newContact.first = $('#first-name').val();
-        newContact.last = $('#last-name').val();
-        newContact.phone = $('#phone-number').val();
-        newContact.street = $('#street').val();
-        newContact.city = $('#city').val();
-        newContact.state = $('#state').val();
-        newContact.zip = $('#zipcode').val();
-        contacts.push(newContact);
-        //Variable to hold first and last name
-        var contactInfo = newContact.first + " " + newContact.last;
-        //function to display (full info) property to added object
-        newContact.display = function() {
-            $('.first-last').html(contactInfo);
-            $('.first').html('First Name: ' + this.first);
-            $('.last').html('Last Name: ' + this.last);
-            $('.phone').html('Phone Number: ' + this.phone);
-            $('.address').html('Address: ' + '<ul><li>' + this.street + ", " + this.city + ", " + this.state + '</ul></li>');
-        }
-        //calling the display function inside the addContact function
-        contactDisplay(contactInfo, function() {
-            //passing this to the specific contact being added
-            newContact.display();
-        });
-        $('#form')[0].reset();
-    } //addContact End
+	//FUNCTIONS
+	//Adding a contact
+	function addContact() {
+		var newContact = Object.create(emptyContact);
+		newContact.first = $('#first-name').val();
+		newContact.last = $('#last-name').val();
+		newContact.phone = $('#phone-number').val();
+		newContact.street = $('#street').val();
+		newContact.city = $('#city').val();
+		newContact.state = $('#state').val();
+		newContact.zip = $('#zipcode').val();
+		contacts.push(newContact);
+		//Variable to hold first and last name
+		var contactInfo = newContact.first + " " + newContact.last;
+		//function to display (full info) property to added object
+		newContact.display = function() {
+			$('.first-last').html(contactInfo);
+			$('.first').html('First Name: ' + this.first);
+			$('.last').html('Last Name: ' + this.last);
+			$('.phone').html('Phone Number: ' + this.phone);
+			$('.address').html('Address: ' + '<ul><li>' + this.street + ", " + this.city + ", " + this.state + '</ul></li>');
+		}
+		//calling the display function inside the addContact function
+		contactDisplay(contactInfo, function() {
+			//passing this to the specific contact being added
+			newContact.display();
+		});
+		$('#form')[0].reset();
+	} //addContact End
 
-    function contactDisplay(contactInfo, clickHandler) {
-        var aTag = $("<li> <a class='link' href='#display-area'>" + contactInfo + "</a> </li>").click(clickHandler);
-        $('.contact-list').append(aTag)
-    }
+	function contactDisplay(contactInfo, clickHandler) {
+		var aTag = $("<li> <a class='link' href='#display-area'>" + contactInfo + "</a> </li>").click(clickHandler);
+		$('.contact-list').append(aTag)
+	}
 
-    $('#form').submit(function(event) {
-        event.preventDefault();
-        addContact();
-    })
+	$('#form').submit(function(event) {
+		event.preventDefault();
+		addContact();
+	})
 
 }); // document ready close
